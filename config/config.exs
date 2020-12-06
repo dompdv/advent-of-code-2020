483fe2c2bc738118832bfcff41ce7adbe45ae353
Dominique Poudevigne <dominique.poudevigne@dataiku.com>
2020-12-06 20:10:19 +0100

    day01_p1 done

--- config/config.exs
+++ config/config.exs
@@ -1,17 +1,5 @@
 use Mix.Config
 
 config :advent_of_code, AdventOfCode.Input,
-  # allow_network?: true,
-  session_cookie: System.get_env("ADVENT_OF_CODE_SESSION_COOKIE")
-
-# If you don't like environment variables, put your cookie in
-# a `config/secrets.exs` file like this:
-#
-# config :advent_of_code, AdventOfCode.Input,
-#   advent_of_code_session_cookie: "session=..."
-
-try do
-  import_config "secrets.exs"
-rescue
-  _ -> :ok
-end
+  allow_network?: true,
+  session_cookie: "53616c7465645f5f6f4f637c561698611062e888d2de85a778787b284a140caa6a6b85983be17db818be4665e5e25868"
--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,7 +1,27 @@
 defmodule AdventOfCode.Day01 do
   def part1(args) do
+    expenses = Enum.sort(parse_input(args))
+    # Enum.map(expenses, &(IO.inspect(&1)))
+    narrow(expenses, 2020)
   end
 
-  def part2(args) do
+  def part2(_args) do
+  end
+
+  defp parse_input(args) do
+    String.split(args)
+    |> Enum.map(&Integer.parse(&1))
+    |> Enum.map(&elem(&1,0))
+  end
+
+  defp narrow(expenses, target) do
+    f = List.first(expenses)
+    l = List.last(expenses)
+    t =  f + l
+    cond do
+      t == target -> f * l
+      t < target -> narrow(Enum.slice(expenses, 1..-1), target)
+      true -> narrow(Enum.slice(expenses, 0..-2), target)
+    end
   end
 end
