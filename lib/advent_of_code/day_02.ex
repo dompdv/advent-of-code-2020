89f183baaa850af911af3188fb653ea5bc3b62d0
Dominique Poudevigne <dominique.poudevigne@dataiku.com>
2020-12-07 22:34:43 +0100

    day2 p2

--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -4,12 +4,20 @@ defmodule AdventOfCode.Day02 do
     Enum.count(passwords, &is_valid_p1(&1))
   end
 
-  def part2(_args) do
+  def part2(args) do
+    passwords = parse_input(args)
+    Enum.count(passwords, &is_valid_p2(&1))
   end
 
   defp is_valid_p1({low, high, letter, password}) do
     number = Enum.count(String.graphemes(password), fn c -> c == letter end)
     low <= number && number <= high
+  end
+
+  defp is_valid_p2({low, high, letter, password}) do
+    first = String.at(password, low - 1) == letter
+    last =  String.at(password, high - 1) == letter
+    (first or last) and (not (first and last))
   end
 
   defp parse_line([a, b, c]) do
