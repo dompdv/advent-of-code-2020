e4357ba7bf3f342ba983e3fcbaa5e767e37e30bf
Dominique Poudevigne <dominique.poudevigne@dataiku.com>
2020-12-07 22:25:48 +0100

    day2 p1

--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -1,7 +1,25 @@
 defmodule AdventOfCode.Day02 do
   def part1(args) do
+    passwords = parse_input(args)
+    Enum.count(passwords, &is_valid_p1(&1))
   end
 
-  def part2(args) do
+  def part2(_args) do
+  end
+
+  defp is_valid_p1({low, high, letter, password}) do
+    number = Enum.count(String.graphemes(password), fn c -> c == letter end)
+    low <= number && number <= high
+  end
+
+  defp parse_line([a, b, c]) do
+    [{low, _}, {high, _}] = String.split(a, "-") |> Enum.map(&Integer.parse/1)
+    {low, high, String.at(b, 0), c}
+  end
+
+  defp parse_input(args) do
+    String.split(args)
+    |> Enum.chunk_every(3)
+    |> Enum.map(&parse_line/1)
   end
 end
