15c9b363da31b006406285eed54bb9b5902a2ccb
Dominique Poudevigne <dominique.poudevigne@dataiku.com>
2020-12-07 19:50:34 +0100

    day1 p2

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -2,10 +2,14 @@ defmodule AdventOfCode.Day01 do
   def part1(args) do
     expenses = Enum.sort(parse_input(args))
     # Enum.map(expenses, &(IO.inspect(&1)))
-    narrow(expenses, 2020)
+    {:ok, result} = narrow(expenses, 2020)
+    result
   end
 
-  def part2(_args) do
+  def part2(args) do
+    expenses = Enum.sort(parse_input(args))
+    {:ok, result} = scan(expenses, 2020)
+    result
   end
 
   defp parse_input(args) do
@@ -14,12 +18,28 @@ defmodule AdventOfCode.Day01 do
     |> Enum.map(&elem(&1,0))
   end
 
+  defp scan([], _target) do
+    :nil
+  end
+
+  defp scan([h | r], target) do
+    case narrow(r, target - h)  do
+       {:ok, result} -> {:ok, h * result}
+       _ -> scan(r, target)
+    end
+
+  end
+
+  defp narrow([], _target) do
+    :nil
+  end
+
   defp narrow(expenses, target) do
     f = List.first(expenses)
     l = List.last(expenses)
     t =  f + l
     cond do
-      t == target -> f * l
+      t == target -> {:ok, f * l}
       t < target -> narrow(Enum.slice(expenses, 1..-1), target)
       true -> narrow(Enum.slice(expenses, 0..-2), target)
     end
