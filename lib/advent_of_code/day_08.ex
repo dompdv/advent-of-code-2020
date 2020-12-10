2b778bd4f3a479351872c99237f6a5593162efd8
Dominique Poudevigne <dominique.poudevigne@dataiku.com>
2020-12-10 23:36:32 +0100

    d08.p1

--- lib/advent_of_code/day_08.ex
+++ lib/advent_of_code/day_08.ex
@@ -1,7 +1,60 @@
 defmodule AdventOfCode.Day08 do
   def part1(args) do
+    program = parse(args)
+    Stream.iterate(load(program), &execute/1)
+    |> Stream.drop_while(&running?/1)
+    |> Enum.take(1)
+    |> Enum.at(0)
+    |> acc()
   end
 
-  def part2(args) do
+  def part2(_args) do
+  end
+
+  defp load(program) do
+    %{program: program, pc: 0, visited: MapSet.new(), acc: 0, running: true}
+  end
+
+  def running?(%{running: running}) do
+    running
+  end
+
+  defp acc(%{acc: acc}) do
+    acc
+  end
+
+  defp execute(%{program: program, pc: pc, visited: visited, acc: acc, running: running}) do
+    if not running do
+      %{program: program, pc: pc, visited: visited, acc: acc, running: false}
+    else
+        if MapSet.member?(visited, pc) do
+          %{program: program, pc: pc, visited: visited, acc: acc, running: false}
+        else
+          {op, par} = Enum.at(program, pc)
+          case op do
+            :nop -> %{program: program, pc: pc + 1, visited: MapSet.put(visited, pc), acc: acc, running: true}
+            :acc -> %{program: program, pc: pc + 1, visited: MapSet.put(visited, pc), acc: acc + par, running: true}
+            :jmp -> %{program: program, pc: pc + par, visited: MapSet.put(visited, pc), acc: acc , running: true}
+          end
+        end
+    end
+  end
+
+  defp parse_line(line) do
+    [cmd, parameter] = line |> String.split(" ")
+    { case cmd do
+      "nop" -> :nop
+      "acc" -> :acc
+      "jmp" -> :jmp
+    end,
+      String.to_integer(parameter)
+    }
+  end
+
+  defp parse(input) do
+    input
+    |> String.split("\n")
+    |> Enum.filter(&(&1 != ""))
+    |> Enum.map(&parse_line/1)
   end
 end
--- lib/mix/tasks/d08.p1.ex
+++ lib/mix/tasks/d08.p1.ex
@@ -5,8 +5,16 @@ defmodule Mix.Tasks.D08.P1 do
 
   @shortdoc "Day 08 Part 1"
   def run(args) do
-    input = nil
-
+    input = "nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6"
+input = AdventOfCode.Input.get!(8, 2020)
     if Enum.member?(args, "-b"),
       do: Benchee.run(%{part_1: fn -> input |> part1() end}),
       else:
