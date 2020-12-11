42391a55149135e0249185fe0e4653c672fda9e5
Dominique Poudevigne <dominique.poudevigne@dataiku.com>
2020-12-12 00:50:43 +0100

    d10.p2

--- lib/advent_of_code/day_10.ex
+++ lib/advent_of_code/day_10.ex
@@ -13,7 +13,49 @@ defmodule AdventOfCode.Day10 do
     jolt_diff[1] * jolt_diff[3]
   end
 
-  def part2(_args) do
+  def part2(args) do
+      # Parse & sort
+      adapters = parse(args) |> Enum.sort()
+      # Add the plug and the adapter in the bag
+      adapters = List.insert_at([0 | adapters], -1, Enum.max(adapters) + 3) |> MapSet.new()
+      #possibilities_to_get_to Enum.max(adapters), adapters
+      max_value = Enum.max(adapters)
+      acc = possibilities Map.new(0..max_value, fn x -> if x== 0 do {x, 1} else {x, 0} end end), MapSet.new(adapters), Enum.to_list(0..max_value)
+      acc[max_value]
+  end
+
+  defp possibilities(acc, _adapters, []) do
+    acc
+  end
+
+  defp possibilities(acc, adapters, [h | rest]) do
+    v = acc[h]
+    acc = if MapSet.member?(adapters, h + 1) do
+      Map.put(acc, h + 1, v + Map.get(acc, h + 1))
+    else
+      acc
+    end
+    acc = if MapSet.member?(adapters, h + 2) do
+      Map.put(acc, h + 2, v + Map.get(acc, h + 2))
+    else
+      acc
+    end
+    acc = if MapSet.member?(adapters, h + 3) do
+      Map.put(acc, h + 3, v + Map.get(acc, h + 3))
+    else
+      acc
+    end
+    possibilities acc, adapters, rest
+  end
+
+  defp possibilities_to_get_to(target, adapters) do
+    cond do
+      target < 0 -> 0
+      target == 0 -> 1
+      not MapSet.member?(adapters, target) -> 0
+      true -> possibilities_to_get_to(target - 1 , adapters) + possibilities_to_get_to(target - 2, adapters) + possibilities_to_get_to(target - 3, adapters)
+
+    end
   end
 
   defp parse(input) do
