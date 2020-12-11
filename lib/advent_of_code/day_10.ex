b86c4b801c04fc0bff89e20ff1f0749476e3dcef
Dominique Poudevigne <dominique.poudevigne@dataiku.com>
2020-12-11 20:11:59 +0100

    d10.p1

--- lib/advent_of_code/day_10.ex
+++ lib/advent_of_code/day_10.ex
@@ -1,7 +1,25 @@
 defmodule AdventOfCode.Day10 do
   def part1(args) do
+    # Parse & sort
+    adapters = parse(args) |> Enum.sort()
+    # Add the plug and the adapter in the bag
+    adapters = List.insert_at([0 | adapters], -1, Enum.max(adapters) + 3)
+    # Create a list of tuple with 2 consecutive adapters
+    [_| rest] = adapters
+    jolt_diff = Enum.zip(adapters, rest) |> Enum.map(fn {l, h} -> h - l end)
+    # Compute frequencies
+    |> Enum.frequencies()
+    # Et voilou
+    jolt_diff[1] * jolt_diff[3]
   end
 
-  def part2(args) do
+  def part2(_args) do
+  end
+
+  defp parse(input) do
+    input
+    |> String.split("\n")
+    |> Enum.filter(&(&1 != ""))
+    |> Enum.map(fn s -> String.trim(s) |> String.to_integer() end)
   end
 end
