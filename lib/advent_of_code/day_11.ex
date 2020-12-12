bf4d425a00c405e4bac3cba9ff7de275c90cba13
Dominique Poudevigne <dominique.poudevigne@dataiku.com>
2020-12-12 16:02:38 +0100

    d11.p2

--- lib/advent_of_code/day_11.ex
+++ lib/advent_of_code/day_11.ex
@@ -1,29 +1,33 @@
 defmodule AdventOfCode.Day11 do
   def part1(args) do
-    waiting_area = parse(args)
-    initial_state = (for i <- Map.keys(waiting_area), do: {i, 0}) |> Map.new()
-    [{_, _, stable_state}] =
-      Stream.iterate({waiting_area, true, initial_state}, &tick/1)
-      |> Stream.drop_while(fn {_, modified, _} -> modified end)
-      |> Enum.take(1)
-    Enum.sum(Map.values(stable_state))
+    parse1(args) |> run_until_stabilization(4)
  end
 
-  def part2(_rgs) do
+  def part2(args) do
+    parse2(args) |> run_until_stabilization(5)
   end
 
-  defp tick({area, _ , state}) do
+  defp run_until_stabilization(waiting_area, min_seats) do
+    initial_state = (for i <- Map.keys(waiting_area), do: {i, 0}) |> Map.new()
+    [{_, _, stable_state, _}] =
+      Stream.iterate({waiting_area, true, initial_state, min_seats}, &tick/1)
+      |> Stream.drop_while(fn {_, modified, _, _} -> modified end)
+      |> Enum.take(1)
+    Enum.sum(Map.values(stable_state))
+  end
+
+  defp tick({area, _ , state, min_seats}) do
     new_state =
       (for i <- Map.keys(area) do
       adjacent_seats = Enum.sum(Enum.map(area[i], fn seat -> state[seat] end))
       cond do
         state[i] == 0 and adjacent_seats == 0 -> {i, 1, true}
-        state[i] == 1 and adjacent_seats >= 4 -> {i, 0, true}
+        state[i] == 1 and adjacent_seats >= min_seats -> {i, 0, true}
         true -> {i, state[i], false}
       end
     end)
     modified = Enum.any?(new_state, fn {_, _, m} -> m end)
-    {area, modified, Enum.map(new_state, fn {i, v, _} -> {i, v} end) |> Map.new()}
+    {area, modified, Enum.map(new_state, fn {i, v, _} -> {i, v} end) |> Map.new(), min_seats}
   end
 
   defp is_seat(input, rows, cols, r, c) do
@@ -49,15 +53,53 @@ defmodule AdventOfCode.Day11 do
     |> Map.new()
   end
 
-  defp parse(input) do
-    raw_input =
+  defp parse_raw(input) do
       input
       |> String.split("\n")
       |> Enum.filter(&(&1 != ""))
       |> Enum.map(fn s -> String.trim(s) end)
-      rows = Enum.count(raw_input)
-      cols = String.length(List.first(raw_input))
-      convert_to_ds(rows, cols, raw_input)
+  end
+
+  defp parse1(input) do
+    raw_input = parse_raw(input)
+    rows = Enum.count(raw_input)
+    cols = String.length(List.first(raw_input))
+    convert_to_ds(rows, cols, raw_input)
+  end
+
+  defp is_seat_in_direction(input, rows, cols, r, c, dr, dc) do
+    m = max(rows, cols)
+    n = 1..m |> Enum.drop_while(fn n -> not is_seat(input, rows, cols, r + n * dr, c + n *dc) end)
+    cond do
+      Enum.count(n) == 0 -> {false, 0, 0}
+      true ->
+        n = hd(n)
+        {true, r + n * dr, c + n * dc}
+    end
+
+  end
+
+  defp convert_to_ds2(rows, cols, input) do
+    for r <- 0..(rows - 1), c <- 0..(cols - 1), is_seat(input, rows, cols, r, c)  do
+      adjacent_seats =
+            Enum.reduce(
+              [{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}],
+              [],
+              fn {dr, dc}, acc ->
+                  {is_seat, sr, sc} = is_seat_in_direction(input, rows, cols, r, c, dr, dc)
+                  if is_seat, do: [sr * cols + sc | acc], else: acc
+              end
+            )
+      {r * cols + c, adjacent_seats}
+    end
+    |> Map.new()
+  end
+
+  defp parse2(input) do
+    raw_input = parse_raw(input)
+    rows = Enum.count(raw_input)
+    cols = String.length(List.first(raw_input))
+    convert_to_ds2(rows, cols, raw_input)
   end
 
 end
--- lib/mix/tasks/d11.p2.ex
+++ lib/mix/tasks/d11.p2.ex
@@ -6,16 +6,6 @@ defmodule Mix.Tasks.D11.P2 do
   @shortdoc "Day 11 Part 2"
   def run(args) do
     input = AdventOfCode.Input.get!(11, 2020)
-    input = "L.LL.LL.LL
-    LLLLLLL.LL
-    L.L.L..L..
-    LLLL.LL.LL
-    L.LL.LL.LL
-    L.LLLLL.LL
-    ..L.L.....
-    LLLLLLLLLL
-    L.LLLLLL.L
-    L.LLLLL.LL"
 
 
     if Enum.member?(args, "-b"),
