8d065a1c3c2840ee2bbdcf5e0fc7fd30ee5b933b
Dominique Poudevigne <dominique.poudevigne@dataiku.com>
2020-12-19 17:31:04 +0100

    d16.p2

--- lib/mix/tasks/d16.p2.ex
+++ lib/mix/tasks/d16.p2.ex
@@ -5,18 +5,8 @@ defmodule Mix.Tasks.D16.P2 do
 
   @shortdoc "Day 16 Part 2"
   def run(args) do
-    _input = AdventOfCode.Input.get!(16, 2020)
-    input = "class: 0-1 or 4-19
-row: 0-5 or 8-19
-seat: 0-13 or 16-19
 
-your ticket:
-11,12,13
-
-nearby tickets:
-3,9,18
-15,1,5
-5,14,9"
+    input = AdventOfCode.Input.get!(16, 2020)
     if Enum.member?(args, "-b"),
       do: Benchee.run(%{part_2: fn -> input |> part2() end}),
       else:
