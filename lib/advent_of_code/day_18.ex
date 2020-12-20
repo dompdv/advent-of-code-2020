73ea22c47e55245b6985e0b6b4f0f209e4af047f
Dominique Poudevigne <dominique.poudevigne@dataiku.com>
2020-12-20 08:50:11 +0100

    d18.p2

--- lib/advent_of_code/day_18.ex
+++ lib/advent_of_code/day_18.ex
@@ -63,6 +63,68 @@ defmodule AdventOfCode.Day18 do
     end
   end
 
-  def part2(_args) do
+  def part2(args) do
+    input = args |> String.split("\n") |> Enum.filter(&(&1 != "")) |> Enum.map(&String.trim/1)
+    results = input |> Enum.map(&evaluate_a_line2/1)
+    # Enum.zip(input, results) |> Enum.map(&IO.inspect/1)
+    results |> Enum.sum()
   end
+
+  def evaluate_a_line2(args) do
+     {_, tokens,_} = Enum.reduce(String.graphemes(args) ++ [" "], {[], [], :starting}, &tokenize/2)
+     reduce_expr(tokens)
+  end
+
+  def reduce_expr([]), do: :err
+  def reduce_expr([{:num, op}]), do: op
+
+  def reduce_expr(expr) do
+    {expr, modified1} = reduce_plus(expr, [], false)
+    {expr, modified2} = reduce_par(expr, [], false)
+    if modified1 or modified2 do
+      reduce_expr(expr)
+    else
+      is_plus = Enum.any?(expr, fn token -> token == :plus end)
+      {expr, modified} = reduce_mul(expr, [], false, is_plus)
+      if modified, do: reduce_expr(expr), else: expr
+    end
+  end
+  def reduce_plus([], acc, modified), do: {Enum.reverse(acc), modified}
+  def reduce_plus([{:num, op1}, :plus, {:num, op2}|rest], acc, _) do
+    reduce_plus([{:num, op1 + op2} | rest], acc, true)
+  end
+  def reduce_plus([h|rest], acc, modified) do
+    reduce_plus(rest, [h|acc], modified)
+  end
+
+  def reduce_par([], acc, modified), do: {Enum.reverse(acc), modified}
+  def reduce_par([:op, {:num, op}, :cl|rest], acc, _) do
+    reduce_par([{:num, op} | rest], acc, true)
+  end
+  def reduce_par([h|rest], acc, modified) do
+    reduce_par(rest, [h|acc], modified)
+  end
+
+  def reduce_mul([], acc, modified, _), do: {Enum.reverse(acc), modified}
+
+  def reduce_mul([:mul, {:num, op1}, :mul, {:num, op2}, :mul|rest], acc, _, is_plus) do
+    reduce_mul([:mul, {:num, op1 * op2}, :mul | rest], acc, true, is_plus)
+  end
+  def reduce_mul([:op, {:num, op1}, :mul, {:num, op2}, :mul|rest], acc, _, is_plus) do
+    reduce_mul([:op, {:num, op1 * op2}, :mul | rest], acc, true, is_plus)
+  end
+
+
+  def reduce_mul([:op, {:num, op1}, :mul, {:num, op2}, :cl|rest], acc, _, is_plus) do
+    reduce_mul([{:num, op1 * op2} | rest], acc, true, is_plus)
+  end
+
+  def reduce_mul([{:num, op1}, :mul, {:num, op2}], acc, _, false) do
+    reduce_mul([{:num, op1 * op2}], acc, true, false)
+  end
+
+  def reduce_mul([h|rest], acc, modified, is_plus) do
+    reduce_mul(rest, [h|acc], modified, is_plus)
+  end
+
 end
